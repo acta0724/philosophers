23356761f2c235f47e84cc2558b8352b5136b69c
acta0724 <iwasakatsuya@icloud.com>
2025-04-18 19:51:39 +0900

    [split] monitor_death

--- src/monitor_death.c
+++ src/monitor_death.c
@@ -6,24 +6,45 @@
 /*   By: iwasakatsuya <iwasakatsuya@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 18:43:26 by iwasakatsuy       #+#    #+#             */
-/*   Updated: 2025/04/18 19:33:55 by iwasakatsuy      ###   ########.fr       */
+/*   Updated: 2025/04/18 19:49:05 by iwasakatsuy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
+
+static int check_death(t_philo *philos, t_rules *rules)
+{
+	int	i;
+	int finish;
+
+	i = 0;
+	finish = 0;
+	while (i < rules->num_philo)
+	{
+		pthread_mutex_lock(&(rules->eat_count_lock));
+		if (philos[i].eat_count >= rules->must_eat_count)
+			finish++;
+		pthread_mutex_unlock(&(rules->eat_count_lock));
+		i++;
+	}
+	if (finish == rules->num_philo)
+	{
+		pthread_mutex_lock(&(rules->death_lock));
+		rules->died = 2;
+		pthread_mutex_unlock(&(rules->death_lock));
+		return (1);
+	}
+	return (0);
+}
 
 void	*monitor_death(void *arg)
 {
 	t_philo	*philos;
 	t_rules	*rules;
 	int		i;
-	int		finish;
 
 	philos = (t_philo *)arg;
 	rules = philos[0].rules;
-		// usleep(1);
-	// rules->start_time = get_timestamp();
-	// usleep(rules->time_to_eat);
 	while (!rules->died)
 	{
 		i = 0;
@@ -54,29 +75,8 @@ void	*monitor_death(void *arg)
 				pthread_mutex_unlock(&(rules->last_eat_lock));
 			i++;
 		}
-		if (rules->must_eat_count > 0 && !rules->died)
-		{
-			finish = 0;
-			i = 0;
-			while (i < rules->num_philo)
-			{
-				pthread_mutex_lock(&(rules->finish_lock));
-				pthread_mutex_lock(&(rules->eat_count_lock));
-				if (philos[i].eat_count >= rules->must_eat_count)
-					finish++;
-				pthread_mutex_unlock(&(rules->eat_count_lock));
-				pthread_mutex_unlock(&(rules->finish_lock));
-				i++;
-			}
-			if (finish == rules->num_philo)
-			{
-				pthread_mutex_lock(&(rules->print_lock));
-				rules->died = 2;
-				printf("All philosophers have eaten enough.\n");
-				pthread_mutex_unlock(&(rules->print_lock));
-				break ;
-			}
-		}
+		if (check_death(philos, rules))
+			return (NULL);
 	}
 	return (NULL);
 }
